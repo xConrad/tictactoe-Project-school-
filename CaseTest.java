974f70b97afa1c3dfc0182242ea1f8125bd603b4
Nicolas Lemire <nicolas.lemire@live.fr>
2014-09-26 12:04:49 -0400

    Create CaseTest.java

---
+++ CaseTest.java
@@ -0,0 +1,74 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CaseTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCouleur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCouleur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOccupee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOccupee() {
+		fail("Not yet implemented");
+	}
+
+}
