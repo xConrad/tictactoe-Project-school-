27448be6c1951892f233f3cef171200ece3c9439
Nicolas Lemire <nicolas.lemire@live.fr>
2014-09-26 12:05:35 -0400

    Create GrilleTest.java

---
+++ GrilleTest.java
@@ -0,0 +1,74 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GrilleTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGrille() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGrille() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGrille() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsRemplie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRemplie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAfficherConsole() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCasesPossibles() {
+		fail("Not yet implemented");
+	}
+
+}
