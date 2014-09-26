7500a41d9cdb7520fc176f8689a9ee3af159951a
Nicolas Lemire <nicolas.lemire@live.fr>
2014-09-26 12:05:14 -0400

    Create JoueurTest.java

---
+++ JoueurTest.java
@@ -0,0 +1,84 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JoueurTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testJoueur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCouleur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCouleur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCaseOccupees() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCaseOccupees() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNbJetons() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNbJetons() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGagne() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJouer() {
+		fail("Not yet implemented");
+	}
+
+}
