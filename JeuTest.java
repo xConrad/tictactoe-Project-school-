cd4d573cbc2dc7dd6f3481179f9eda538565e25d
Nicolas Lemire <nicolas.lemire@live.fr>
2014-09-26 12:04:24 -0400

    Create JeuTest.java

---
+++ JeuTest.java
@@ -0,0 +1,69 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JeuTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testJeu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGrille() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGrille() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJoueur1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetJoueur1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJoueur2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetJoueur2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJouer() {
+		fail("Not yet implemented");
+	}
+
+}
